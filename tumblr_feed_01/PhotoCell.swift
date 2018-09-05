46396ba43b0cbd535a08225cffc98d23e481a6da
paul <paul@sfs-wifi-dhcp-10-143-148-103.wifi.sfsu.edu>
2018-09-04 19:35:19 -0700

    MileStone 3 Complete

---
+++ tumblr_feed_01/PhotoCell.swift
@@ -0,0 +1,27 @@
+//
+//  PhotoCell.swift
+//  tumblr_feed_01
+//
+//  Created by paul on 9/4/18.
+//  Copyright © 2018 PoHung Wang. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+
+}
